565ad75a409eaf46c196a48bb3008b57603d8da9
Praveen-Singh0 <108598845+Praveen-Singh0@users.noreply.github.com>
2026-01-17 20:16:11 +0530

    Update heroSection.tsx

--- Frontend/src/app/(Public)/pages/heroSection.tsx
+++ Frontend/src/app/(Public)/pages/heroSection.tsx
@@ -62,12 +62,10 @@ const TopPage = () => {
       <div className="mt-12 p-0 md:p-12">
         <div className="relative isolate px-4 sm:px-5 lg:px-8">
           <div className="flex flex-col lg:flex-row justify-between items-center gap-8 mx-auto max-w-7xl">
-            {/* LEFT CONTENT */}
             <div className="sm:w-8/12">
-              {/* 1️⃣ FIRST DIV */}
               <motion.div
                 {...fadeUp(0.1)}
-                className="sm:flex sm:justify-center"
+                className="flex justify-center"
               >
                 <div className="relative rounded-full px-3 py-1 text-sm text-gray-700 dark:text-gray-100 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   Connect me on LinkedIn.{" "}
@@ -173,7 +171,7 @@ const TopPage = () => {
                   <div className="hero-image-wrapper border border-red-500 dark:border-gray-100">
                     {info?.profileImage && (
                       <Image
-                        src={info.profileImage}
+                        src={info?.profileImage}
                         alt="Avatar"
                         width={350}
                         height={350}
