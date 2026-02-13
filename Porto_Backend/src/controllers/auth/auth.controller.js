10e89021ce6108c622bdbe4888602e64838efc13
Praveen-Singh0 <108598845+Praveen-Singh0@users.noreply.github.com>
2026-02-13 11:04:29 +0530

    Update auth.controller.js

--- Porto_Backend/src/controllers/auth/auth.controller.js
+++ Porto_Backend/src/controllers/auth/auth.controller.js
@@ -103,7 +103,7 @@ export const login = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
