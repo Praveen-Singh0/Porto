a37bb1c49c4d25deecc0ff84d0edf38c00b195c1
Praveen-Singh0 <108598845+Praveen-Singh0@users.noreply.github.com>
2026-03-03 19:21:04 +0530

    min

--- Frontend/src/app/(Public)/error/googleAuth/page.tsx
+++ Frontend/src/app/(Public)/error/googleAuth/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function GoogleAuthErrorPage() {
-  const params = useSearchParams();
   const router = useRouter();
 
-  const error = params.get("error") || "Unknown error occurred";
 
   useEffect(() => {
-    console.log("Google Auth Error:", error);
-  }, [error]);
+    console.log("Google Auth Error:");
+  }, []);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-black px-4">
@@ -24,10 +22,6 @@ export default function GoogleAuthErrorPage() {
         <p className="text-gray-700 dark:text-gray-300 mb-6">
           Something went wrong while signing in with Google.
         </p>
-
-        <div className="text-sm text-gray-600 dark:text-gray-400 p-3 mb-6 rounded-lg bg-gray-200 dark:bg-gray-800">
-          Error: <span className="font-semibold">{error}</span>
-        </div>
 
         <button
           onClick={() => router.push("/")}
